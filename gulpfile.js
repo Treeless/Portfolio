d99a0cce7667669f22e025226b2e96380ae70737
Matthew Rowlandson <matt_94101@live.ca>
2017-10-17 12:25:19 -0400

    Added some projects. Tweaked some content

--- gulpfile.js
+++ gulpfile.js
@@ -1,47 +1,48 @@
 (function() {
-    var gulp = require('gulp'),
-        sass = require('gulp-sass'),
-        cssnano = require('gulp-cssnano'),
-        concat = require('gulp-concat'),
-        autoprefix = require('gulp-autoprefixer'),
-        webserver = require('gulp-webserver'),
-        del = require('del');
+  var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    cssnano = require('gulp-cssnano'),
+    concat = require('gulp-concat'),
+    autoprefix = require('gulp-autoprefixer'),
+    webserver = require('gulp-webserver'),
+    del = require('del');
 
-    //Compiles scss files to css
-    gulp.task('style', function() {
-        del.sync('./css/main.css');
+  //Compiles scss files to css
+  gulp.task('style', function() {
+    del.sync('./css/main.css');
 
-        gulp.src('**/*.scss', {
-                cwd: './src'
-            })
-            .pipe(concat('main.scss'))
-            .pipe(sass().on('error', function(err) {
-                console.log("SASS ERROR: " + err.message);
-            }))
-            .pipe(cssnano())
-            .pipe(autoprefix())
-            .pipe(gulp.dest('./css'));
-    });
+    gulp.src('**/*.scss', {
+        cwd: './src'
+      })
+      .pipe(concat('main.scss'))
+      .pipe(sass().on('error', function(err) {
+        console.log("SASS ERROR: " + err.message);
+      }))
+      .pipe(cssnano())
+      .pipe(autoprefix())
+      .pipe(gulp.dest('./css'));
+  });
 
-    //Watch my scss files for changes
-    gulp.task('watch-sass', function() {
-        gulp.watch('**/*.scss', {
-            cwd: "./src/"
-        }, ['style']);
-    });
+  //Watch my scss files for changes
+  gulp.task('watch-sass', function() {
+    gulp.watch('**/*.scss', {
+      cwd: "./src/"
+    }, ['style']);
+  });
 
-    //Local webserver
-    gulp.task('webserver', function() {
-        gulp.src('./')
-            .pipe(webserver({
-                livereload: {
-                    enable: true
-                },
-                fallback: 'index.html',
-                open: true
-            }));
-    });
+  //Local webserver
+  gulp.task('webserver', function() {
+    gulp.src('./')
+      .pipe(webserver({
+        livereload: {
+          enable: true
+        },
+        fallback: 'index.html',
+        open: true,
+        port: 8082
+      }));
+  });
 
-    //Default task
-    gulp.task('default', ['style', 'watch-sass', 'webserver']);
+  //Default task
+  gulp.task('default', ['style', 'watch-sass', 'webserver']);
 }());
