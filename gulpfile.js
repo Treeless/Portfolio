c1d8bea8b005be9a2d8b48fb5109d82f423d1c17
Matthew Rowlandson <matt@fyrespark.com>
2020-07-08 08:48:53 -0400

    upgraded to gulp 4, upgraded other npm packages

--- gulpfile.js
+++ gulpfile.js
@@ -7,32 +7,26 @@
     webserver = require('gulp-webserver'),
     del = require('del');
 
-  //Compiles scss files to css
-  gulp.task('style', function() {
-    del.sync('./css/main.css');
+  // Remove the css file
+  gulp.task("clean-css", () => del(["./css/main.css"]));
 
-    gulp.src('**/*.scss', {
+  //Compiles scss files to css
+  gulp.task('style', gulp.series("clean-css", function convertSassToCss() {
+    return gulp.src('**/*.scss', {
         cwd: './src'
       })
       .pipe(concat('main.scss'))
-      .pipe(sass().on('error', function(err) {
+      .pipe(sass().on('error', function sassError(err) {
         console.log("SASS ERROR: " + err.message);
       }))
       .pipe(cssnano())
       .pipe(autoprefix())
       .pipe(gulp.dest('./css'));
-  });
-
-  //Watch my scss files for changes
-  gulp.task('watch-sass', function() {
-    gulp.watch('**/*.scss', {
-      cwd: "./src/"
-    }, ['style']);
-  });
+  }));
 
   //Local webserver
-  gulp.task('webserver', function() {
-    gulp.src('./')
+  gulp.task('webserver', gulp.series(function setupWebserver() {
+    return gulp.src('./')
       .pipe(webserver({
         livereload: {
           enable: true
@@ -41,8 +35,14 @@
         open: true,
         port: 8082
       }));
-  });
+  }));
 
   //Default task
-  gulp.task('default', ['style', 'watch-sass', 'webserver']);
-}());
+  gulp.task('default', gulp.series('style', 'webserver', (done) => {
+
+    //Watch sass
+    gulp.watch("**/*.scss", { cwd: "./src/" }, gulp.series("style"))
+
+    done();
+  }));
+}()); 
