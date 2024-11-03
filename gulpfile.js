0e798eb45b254e48b625137b566611181034e7ee
Matthew Rowlandson <matt_94101@live.ca>
2024-11-02 21:00:21 -0400

    replace gulp-webserver with gulp-connect and gulp-open

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,18 @@ import gulpSass from 'gulp-sass';
 import cssnano from 'gulp-cssnano';
 import concat from 'gulp-concat';
 import autoprefixer from 'gulp-autoprefixer';
-import gulpWebserver from 'gulp-webserver';
-import * as sassCompiler from 'sass';  // Updated to match the suggested import style
+import connect from 'gulp-connect';
+import * as sassCompiler from 'sass';
+import open from 'gulp-open';
 
 const sass = gulpSass(sassCompiler);
+const port = 8082;
+const uri = `http://localhost:${port}/`;
 
 // Remove the CSS file
 gulp.task("clean-css", (done) => {
   deleteSync(["./css/main.css"]);
-  done(); // Signal task completion
+  done();
 });
 
 // Compiles SCSS files to CSS
@@ -24,26 +27,31 @@ gulp.task('style', gulp.series("clean-css", function convertSassToCss() {
     }))
     .pipe(cssnano())
     .pipe(autoprefixer())
-    .pipe(gulp.dest('./css'));
+    .pipe(gulp.dest('./css'))
+    .pipe(connect.reload()); // Reload the server when CSS changes
 }));
 
 // Local webserver
-gulp.task('webserver', gulp.series(function setupWebserver(done) {
-  gulp.src('./')
-    .pipe(gulpWebserver({
-      livereload: {
-        enable: true
-      },
-      fallback: 'index.html',
-      open: true,
-      port: 8082
-    }));
-  done(); // Signal task completion
-}));
+gulp.task('webserver', (done) => {
+  connect.server({
+    root: './',
+    livereload: true,
+    fallback: 'index.html',
+    port: port
+  });
+  done();
+});
+
+// Open browser
+gulp.task('open-browser', (done) => {
+  gulp.src('index.html')
+    .pipe(open({ uri }));
+  done();
+});
 
 // Default task
-gulp.task('default', gulp.series('style', 'webserver', (done) => {
+gulp.task('default', gulp.series('style', 'webserver', 'open-browser', (done) => {
   // Watch Sass files
   gulp.watch("**/*.scss", { cwd: "./src/" }, gulp.series("style"));
-  done(); // Signal task completion
+  done();
 }));
